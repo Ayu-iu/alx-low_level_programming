6c25352286d59d2cce125bb376fb851d8f0240e3
Ayu-iu <olgerah.ayantu@gmail.com>
2022-12-13 02:58:01 -0800

    second jab at c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Entry point
- *
- * Description - using printf function
- *
+ * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return(0);
 
