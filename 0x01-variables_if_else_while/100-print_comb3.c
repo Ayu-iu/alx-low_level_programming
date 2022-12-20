7f4acbf2e15bf636a2393784f917d3b1e6a409ec
Ayu-iu <olgerah.ayantu@gmail.com>
2022-12-20 12:12:06 -0800

    advanced task fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-				putchar((num1 % 10) + '0');
-				putchar((num2 % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-				if (num1 == 8 && num2 == 9)
-					continue;
+			if (num1 == 8 && num2 == 9)
+				continue;
 
-				putchar(44);
-				putchar(32);
+			putchar(44);
+			putchar(32);
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
