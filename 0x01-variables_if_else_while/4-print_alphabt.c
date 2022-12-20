cf97aabe2b52252b0d4191838f3815e65a8d0c8a
Ayu-iu <olgerah.ayantu@gmail.com>
2022-12-20 09:27:15 -0800

    print the alphas

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all letters except e and q
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			++ch;
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+	return (0);
+}
