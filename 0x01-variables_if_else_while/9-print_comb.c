9f88a9a4740cbad11f1823357e69883a864416de
Ayu-iu <olgerah.ayantu@gmail.com>
2022-12-20 10:52:09 -0800

    not quite sure

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - print combos of single-digit nums
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int comb;
+
+	for (comb = '0'; comb < '9'; ++comb)
+	{
+		putchar(comb);
+		if (comb != '9')
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
