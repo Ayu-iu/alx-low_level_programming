77395050ce61e482488bacbe6e65392e2f84817d
Ayu-iu <olgerah.ayantu@gmail.com>
2022-12-20 11:11:15 -0800

    advanced task1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - print combos of two digit nums
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; ++num1)
+	{
+		for (num2 = num1 + 1; num2 < 10; ++num2)
+		{
+			if (num2 != num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' && num2 == '9')
+					continue;
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
