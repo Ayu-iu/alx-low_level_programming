47e8fc3dbc11e3717a5a94801b4a739e370fa5e8
Ayu-iu <olgerah.ayantu@gmail.com>
2023-06-08 16:20:01 -0700

    recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,6 +9,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == '\0')
-		return (-1);
+	int start = 1;
+	int end = n / 2;
+	int result = _sqrt_recursion(n, start, end);
+	int mid = (start + end) / 2;
 
+	if (n < 0){
+		return -1;
+	}
+	if (n == 0 || n == 1){
+		return (n);
+	}
+	if (mid * mid == n){
+		return mid;
+	}
+	if (start >= end){
+		return -1;
+	}
+	if (mid * mid > n){
+		return _sqrt_recursion(n, start, mid - 1);
+	}
+	return _sqrt_recursion(n, mid + 1, end);
+}
