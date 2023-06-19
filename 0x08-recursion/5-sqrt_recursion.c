4c94dd8791d1ae4187fdc352f0965ebd5ecf440f
Ayu-iu <olgerah.ayantu@gmail.com>
2023-06-19 02:10:08 -0700

    recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,37 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * _sqrt_recursion - returns the natural sqrt of a num
  * @n: the num its sqrt is supposed to be returend of
+ * @low: The lowest possible square root of n
+ * @high: The highest possible square root of n
  *
  * Return: -1 if n doesn't have natural sqrt, else its natural sqrt
  */
 
+int _sqrt_helper(int n, int low, int high)
+{
+    int mid = (low + high) / 2;
+
+    if (low > high) {
+        return -1;
+    }
+
+    if (mid * mid == n) {
+        return mid;
+    } else if (mid * mid < n) {
+        return _sqrt_helper(n, mid + 1, high);
+    } else {
+        return _sqrt_helper(n, low, mid - 1);
+    }
+}
+
 int _sqrt_recursion(int n)
 {
-	int start = 1;
-	int end = n / 2;
-	int result = _sqrt_recursion(n, start, end);
-	int mid = (start + end) / 2;
+    if (n < 0) {
+        return -1;
+    }
 
-	if (n < 0){
-		return -1;
-	}
-	if (n == 0 || n == 1){
-		return (n);
-	}
-	if (mid * mid == n){
-		return mid;
-	}
-	if (start >= end){
-		return -1;
-	}
-	if (mid * mid > n){
-		return _sqrt_recursion(n, start, mid - 1);
-	}
-	return _sqrt_recursion(n, mid + 1, end);
+    return _sqrt_helper(n, 0, n);
 }
