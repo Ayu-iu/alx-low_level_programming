df3057da50ad1abc505838d4bb17bd4de99f3033
Ayu-iu <olgerah.ayantu@gmail.com>
2023-06-19 02:24:40 -0700

    recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,26 +12,31 @@
 
 int _sqrt_helper(int n, int low, int high)
 {
-    int mid = (low + high) / 2;
-
-    if (low > high) {
-        return -1;
-    }
-
-    if (mid * mid == n) {
-        return mid;
-    } else if (mid * mid < n) {
-        return _sqrt_helper(n, mid + 1, high);
-    } else {
-        return _sqrt_helper(n, low, mid - 1);
-    }
+	int mid = (low + high) / 2;
+	
+	if (low > high) 
+	{
+		return -1;
+	}
+	if (mid * mid == n) 
+	{
+		return mid;
+	} 
+	else if (mid * mid < n) 
+	{
+		return _sqrt_helper(n, mid + 1, high);
+	}
+	else
+	{
+		return _sqrt_helper(n, low, mid - 1);
+	}
 }
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0) {
-        return -1;
-    }
-
-    return _sqrt_helper(n, 0, n);
+	if (n < 0)
+	{
+		return -1;
+	}
+	return _sqrt_helper(n, 0, n);
 }
