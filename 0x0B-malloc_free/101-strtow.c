241b05d7b73542d117cb95822071bcae7ea9ac94
Ayu-iu <olgerah.ayantu@gmail.com>
2023-06-24 15:29:44 -0700

    spit em words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,7 +6,7 @@
  * strtow - split a string into words
  * @str; the string to be splited \ /
  *
- * Return : pointer --> to an arro of z strings, NULL on failure :/
+ * Return : pointer --> to an arry of z strings, NULL on failure :/
  */
 
 char **strtow(char *str)
@@ -24,13 +24,24 @@ char **strtow(char *str)
 
 	for (i = 0; i < len; i++)
 	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			words++;
+	}
+	
+	arr  = malloc(sizeof(char *) * (words + 1));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0, k = 0; i < words; i++)
+	{
 		while (str[k] == ' ')
 			k++;
+
 		len = 0;
 		for (j = k; str[j] && str[j] != ' '; j++)
 			len++;
+		
 		arr[i] = malloc(sizeof(char) * (len + 1));
-
 		if (arr[i] == NULL)
 		{
 			for (i--; i >=0; i--)
@@ -41,10 +52,8 @@ char **strtow(char *str)
 
 		for (j = 0; j < len; j++, k++)
 			arr[i][j] = str[k];
-
 		arr[i][j] = '\0';
 	}
 	arr[i] = NULL;
-
 	return (arr);
 }
