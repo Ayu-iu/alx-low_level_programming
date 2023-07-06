e5c66cac8093c710828426bd85c410e90807c8fc
Ayu-iu <olgerah.ayantu@gmail.com>
2023-07-06 12:34:43 -0700

    main file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putcha(char c);
+void _puts_recursion(char *s);
+void _print_rev_reurison(char *s);
+int _strlen_recurion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int );
+int _sqrt_recursion(int n);
+int is_prime_numbeer(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char* s2);
+
+#endif
