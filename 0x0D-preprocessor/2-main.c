117837f2442ff7156e735e92938e482195087338
Ayu-iu <olgerah.ayantu@gmail.com>
2023-07-17 15:02:54 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,22 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * get_filename - gets the file it was compiled from
- * Return: the file name
+ * _putchar - prints the file it was compiled from
+ * Return: as always 0
  */
 
-char *get_filename(void)
+int main(void)
 {
-	const char *file = __FILE__;
-	size_t len = strlen(file);
-	char *filename = malloc((len + 1) * sizeof(char));
-
-	if (filename != NULL)
-	{
-		memcpy(filename, file, len);
-		filename[len] = '\0';
-	}
-
-	return (filename);
+	_putchar("%s\n", __FILE__);
+	return (0);
 }
