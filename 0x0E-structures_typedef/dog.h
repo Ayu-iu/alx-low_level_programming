5d7eb80c6e39f80a1bacb7de119c31e4439870f0
Ayu-iu <olgerah.ayantu@gmail.com>
2023-07-17 15:32:00 -0700

    struct of a dogo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct representing a dogo
+ * @age: age of dogo
+ * @owner: owner of dogo
+ * @name: name of dogo
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
