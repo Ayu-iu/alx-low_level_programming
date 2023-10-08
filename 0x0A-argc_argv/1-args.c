38f96906c9877ced538971d63241132bff0c4fcc
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-09 01:58:18 +0300

    argument tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,6 @@
 /**
  * main - prints the num of args passed into z program
  * @argc: the num of comandline args
- * @argv: an array of command-line argument strings
  * 
  * Return: as always 0
  */
@@ -13,7 +12,8 @@ int count_arguments(int argc) {
     return (argc - 1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc) 
+{
     printf("%d\n", count_arguments(argc));
     return (0);
 }
