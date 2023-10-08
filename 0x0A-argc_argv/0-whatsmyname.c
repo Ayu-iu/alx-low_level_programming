6bc2bf9974b21dd1e666efe7ea99ce8619fb9349
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-09 01:39:26 +0300

    argument tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: the num of command-line arguments
- * @argv: An array of command-line argument strings
- * 
- * Return: Always 0
-*/
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(int argc, char *argv[])
-{ 
-    int i = 0;
-
-    (void)argc;
-
-    while (argv[0][i] != '\0')
-    {
-        _putchar(argv[0][i]);
-        i++;
-    }
-    putchar('\n');
+int main(void)
+{
+    printf("%s\n", __FILE__);
     return (0);
 }
