a8e0b586bdf2bdd79ec846c795c230f0334e89a9
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 17:16:52 +0300

    betty betty bettyy...

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,7 +26,7 @@ int i, sum = 0;
 if (argc == 1)
 {
 printf("0\n");
-return 0;
+return (0);
 }
 
 for (i = 1; i < argc; i++)
@@ -38,7 +38,7 @@ while (argv[i][j] != '\0')
 if (!is_digit(argv[i][j]))
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 j++;
 }
@@ -48,5 +48,5 @@ sum += atoi(argv[i]);
 
 printf("%d\n", sum);
 
-return 0;
+return (0);
 }
