21c7c35ea4b00c64101cfcbce784e9e66dbd3769
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 15:16:22 +0300

    priming the numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,20 +10,30 @@
 int is_prime_number(int n)
 {
 if (n <= 1)
-return (0);
-else if (n <= 3)
-return (1);
-else if (n % 2 == 0 || n % 3 == 0)
-return (0);
-else
 {
+return (0);
+}
+if (n <= 3)
+{
+return (1);
+}
+if (n % 2 == 0 || n % 3 == 0)
+{
+return (0);
+}
+
 int i = 5;
+int w = 2;
+
 while (i * i <= n)
 {
-if (n % i == 0 || n % (i + 2) == 0)
+if (n % i == 0)
+{
 return (0);
-i += 6;
+}
+i += w;
+w = 6 - w;
 }
 return (1);
 }
-}
+
