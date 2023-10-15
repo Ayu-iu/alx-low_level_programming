eee67045004639f20c5f5e4eb9de6ad70db1b337
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 15:46:40 +0300

    priming over and over

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * is_prime_number - cheks if a number is prime or not
+ * is_prime_recursive - checks if a number is prime usin recursion
  * @n: the num to be checked
- * 
+ * @i: the divisor to be checked
+ * @w: the increment valure for the divisor
  * Return: 1 if n is indeed prime oterwise 0
 */
 int is_prime_recursive(int n, int i, int w)
@@ -11,15 +12,19 @@ int is_prime_recursive(int n, int i, int w)
 i += w;
 w = 6 - w;
 
-if (i * i > n) {
-return 1;
-}
-
-if (n % i == 0) {
-return 0;
-}
+if (i * i > n)
+return (1);
+if (n % i == 0)
+return (0);
 return is_prime_recursive(n, i, w);
 }
+
+/**
+ * is_prime_number - cheks if a number is prime or not
+ * @n: the num to be checked
+ *
+ * Return: 1 if n is indeed prime oterwise 0
+*/
 
 int is_prime_number(int n)
 {
@@ -27,17 +32,11 @@ int i = 5;
 int w = 2;
 
 if (n <= 1)
-{
 return (0);
-}
 if (n == 2 || n == 3)
-{
 return (1);
-}
 if (n % 2 == 0 || n % 3 == 0)
-{
 return (0);
-}
 return is_prime_recursive(n, i, w);
 }
 
