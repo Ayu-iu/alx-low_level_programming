2eeb9050d7d9570a56df4e5f8f5e354db01cb4b7
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 15:18:17 +0300

    comoon primes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,8 +7,12 @@
  * Return: 1 if n is indeed prime oterwise 0
 */
 
+
 int is_prime_number(int n)
 {
+int i = 5;
+int w = 2;
+
 if (n <= 1)
 {
 return (0);
@@ -21,9 +25,6 @@ if (n % 2 == 0 || n % 3 == 0)
 {
 return (0);
 }
-
-int i = 5;
-int w = 2;
 
 while (i * i <= n)
 {
