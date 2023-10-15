49d7023ee8c1b1cf657c445867275d8a229ef60b
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 12:01:32 +0300

    betty's correction

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns value of certain x raised to the power of y
  * @x: the num to be raised to
  * @y: the num that raises x a certain times
- * 
+ *
  * Return: -1 if y is less than 0, else the soln
 */
 
@@ -13,7 +13,7 @@ int _pow_recursion(int x, int y)
 if (y < 0)
 return (-1);
 if (y == 0)
-return(1);
+return (1);
 
 return (x * _pow_recursion(x, y - 1));
 }
