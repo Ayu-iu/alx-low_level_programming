defca6abd98529ea94036e88dfa2971f2b72f58c
Ayu_iu <olgerah.ayantu@gmail.com>
2023-10-15 17:11:10 +0300

    lets try this

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - adds +ve nums
- * @argc: the num of cmd args
- * @argv: the array of cmd arg strings
- * 
- * Return: 0 if sucess, 1 if not\error
-*/
+ * is_digit - Checks if a character is a digit
+ * @c: The character to check
+ *
+ * Return: 1 if the character is a digit, otherwise 0
+ */
+int is_digit(char c)
+{
+    return (c >= '0' && c <= '9');
+}
 
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-    int i, num = 0;
+    int i, sum = 0;
+
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
 
     for (i = 1; i < argc; i++)
     {
-        if (!is_positive_number(argv[i]))
+        int j = 0;
+
+        while (argv[i][j] != '\0')
         {
-            printf("Error\n");
-            return (1);
+            if (!is_digit(argv[i][j]))
+            {
+                printf("Error\n");
+                return 1;
+            }
+            j++;
         }
 
         sum += atoi(argv[i]);
@@ -25,5 +48,5 @@ int main(int argc, char *argv[])
 
     printf("%d\n", sum);
 
-    return (0);
+    return 0;
 }
