203db6744209b17ccc40aef726d371100e075c98
Ayu-iu <olgerah.ayantu@gmial.com>
2024-05-23 22:52:38 +0300

    come on

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,24 +3,15 @@
 /**
  * _isalpha - checks if character is a letter
  * @c: the character to be checked
- * Description: check if character is a letter
+ *
  * Return: 1 if true else 0
  */
 
 int _isalpha(int c)
 {
-<<<<<<< HEAD
-	if ((c >= 'a' && c <= 'z') ||
-	(c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-	return (0);
-=======
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
-		return (1);
-	else
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return (0);
+		return (1);
 	}
->>>>>>> 256d268b4d05f2d40282506ceceb36387cb7344f
+	return (0);
 }
