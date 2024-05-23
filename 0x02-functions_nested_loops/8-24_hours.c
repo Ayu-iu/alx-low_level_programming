73105e1f2f651fb5595e70d9b8e7388ffee27872
Ayu-iu <olgerah.ayantu@gmial.com>
2024-05-23 23:39:31 +0300

    every dang minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+void print_time(int hour, int minute);
+
+/**
+ * jack_bauer - prints every minute of the day from 8-24hrs
+ */
+
+void jack_bauer(void)
+{
+int hour;
+int minute;
+
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+print_time(hour, minute);
+}
+}
+}
+
+/**
+ * print_time - prints a formatted time in HH:MM format
+ * @hour: the current hour
+ * @minute: the current minute
+ */
+
+void print_time(int hour, int minute)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
