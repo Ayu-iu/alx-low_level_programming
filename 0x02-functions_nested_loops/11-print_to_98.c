f0e63904ab1c4cde057c01d1fbbd1e07bd7ca2b8
Ayu-iu <olgerah.ayantu@gmial.com>
2024-06-12 12:49:29 +0300

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,16 @@
 
 void print_to_98(int n)
 {
-    int j;
-    for (j = n;  j >= 0 && j <= 98; j++)
-    {
-        _putchar(j + '0');
+int j;
+for (j = n;  j >= 0 && j <= 98; j++)
+{
+_putchar(j + '0');
 
-        if (j < 98)
-        {
-            _putchar(", ");
-        }
-    }
-    _putchar('\n');
+if (j < 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
 }
