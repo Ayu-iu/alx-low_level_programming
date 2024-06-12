3c6d5de68251dda120b541d83f923e2d0169ff2b
Ayu-iu <olgerah.ayantu@gmial.com>
2024-06-12 12:09:27 +0300

    add dem

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two imput integers
+ * @num1: First integer
+ * @num2: Second integer
+ * 
+ * Return: sum of the two numbers
+*/
+
+int add(int num1, int num2)
+{
+    int sum;
+
+    sum = num1 + num2;
+    return (sum);
+}
